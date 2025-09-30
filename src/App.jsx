d67176df629fe3d903ab7c2491ebc1074ae1b13b
Joice Conte <joiceconte@gmail.com>
2025-09-30 09:01:32 +0200

    updated appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import HomePage from "./pages/HomePage";
 import ContactPage from "./pages/ContactPage";
 import PolicyPage from "./pages/PolicyPage";
 import AboutPage from "./pages/AboutPage";
-import LoginPage from "./pages/LoginPage";
+import LoginPage from "./pages/loginPage";
 import SignUpPage from "./pages/SignUpPage";
 import TripsPage from "./pages/TripsPage";
 import Feed from "./pages/Feed";
@@ -17,7 +17,6 @@ import ProfilePage from "./pages/ProfilePage";
 import SettingsPage from "./pages/SettingsPage.jsx";
 
 import "./App.css";
-import axios from "axios";
 
 export default function App() {
   const location = useLocation();
@@ -28,25 +27,10 @@ export default function App() {
 
   const isHome = location.pathname === "/";
 
-  const handleLogin = async (userData) => {
-    try {
-      const response = await axios.post(
-        "http://localhost:5005/api/auth/login",
-        userData
-      );
-      setIsLoggedIn(true);
-      console.log("userlogedin", response);
-      localStorage.setItem("authToken", response.data.token);
-      setUser(response.data.user);
-      navigate("/");
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   const handleLogout = () => {
     setIsLoggedIn(false);
     setUser({ photo: "" });
+    localStorage.removeItem("token");
     navigate("/");
   };
 
@@ -54,7 +38,7 @@ export default function App() {
     <div className="tm-app">
       <TripMatchNavbar
         variant={isHome ? "home" : "internal"}
-        isLoggedIn={isLoggedIn}
+        isAuthenticated={isLoggedIn}
         onLoginClick={() => navigate("/login")}
         onLogout={handleLogout}
         avatarUrl={user?.photo}
@@ -63,13 +47,24 @@ export default function App() {
       <main className="tm-main">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage onLogin={handleLogin} />} />
+          <Route
+            path="/login"
+            element={
+              <LoginPage
+                onLogin={(user) => {
+                  setIsLoggedIn(true);
+                  setUser(user);
+                  navigate("/");
+                }}
+              />
+            }
+          />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/policy" element={<PolicyPage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/trips" element={<TripsPage user={user} />} />
-          <Route path="/feed" element={<Feed />} />         
+          <Route path="/feed" element={<Feed />} />
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="/settings" element={<SettingsPage />} />
           <Route
